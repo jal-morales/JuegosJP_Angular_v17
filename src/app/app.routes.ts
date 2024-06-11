1449d5ca5eb680242f55c49a25bb3c5426a1ecd2
jal-morales <jal.morales89@gmail.com>
2024-06-10 22:01:06 -0400

    Migracion a  Angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '',redirectTo:'/home', pathMatch:'full'},
+    {path: 'home',component:HomeComponent},
+
+    {path: 'contacto',component:ContactoComponent},
+
+
+
+    {path: '**', redirectTo:'/home'}
+
+
+];
